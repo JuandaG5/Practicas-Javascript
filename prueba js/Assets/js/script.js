2dfd53d1fb56810b4caabb1775b73684943f0c43
JuandaG5 <159591253+JuandaG5@users.noreply.github.com>
2024-04-05 19:43:56 -0500

    Javascript Practice
    Comandos Básicos

---
+++ prueba js/Assets/js/script.js
@@ -0,0 +1,26 @@
+//variables
+let firstName="Juan"
+let lastName
+let numberOne
+let numberTwo
+let numberPhi=3.14
+let state=true
+
+//Métodos de captura de datos
+numberOne=parseInt(prompt("Ingrese un número"))
+numberTwo=parseInt(prompt("Ingrese otro número"))
+
+
+//Métodos de salida de datos
+suma=numberOne + numberTwo
+alert("El número que ingresaste fue " +suma)
+console.log(`El número que ingresaste fue ${suma} Juan`)
+
+//operadores 
+//Aritméticos
+//+, -, *, /, %
+// Relación o comparación --> resultado true o false
+// ==, >=, <=, >, <, !=
+//Lógicos
+// or -->|| and -->&& not -->!
+
